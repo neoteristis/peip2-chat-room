dcb619bee2f78c9ee8b22b9a8d22b8548b2b8269
neoteristis <anjou.raphael@gmail.com>
2022-06-06 17:58:17 +0200

    feat: fix simpleajax warning

--- public_html/js/simpleajax.js
+++ public_html/js/simpleajax.js
@@ -21,16 +21,16 @@ function simpleAjax(url,method,parameters,onSuccess,onFailure) {
 		if ( typeof method != "string" )
 			throw method + ": bad method (choose 'GET' or 'POST')";
 		method = method.trim().toLowerCase();
-		if ( method != 'get' && method != 'post' )
+		if ( method !== 'get' && method !== 'post' )
 			throw method + ": bad method (choose 'GET' or 'POST')";
 		return method;
 	}
 	
 	function orsc(xmlhttp) {
 		return function() {
-			if ( xmlhttp.readyState == 4 && xmlhttp.status == 200 )
+			if ( xmlhttp.readyState === 4 && xmlhttp.status === 200 )
 				onSuccess(xmlhttp);
-			else if ( xmlhttp.readyState == 4 && xmlhttp.status == 404 )
+			else if ( xmlhttp.readyState === 4 && xmlhttp.status === 404 )
 				onFailure(xmlhttp);
 		};
 	}
@@ -41,8 +41,8 @@ function simpleAjax(url,method,parameters,onSuccess,onFailure) {
 	this.xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
 	this.xmlhttp.onreadystatechange = orsc(this.xmlhttp);
 
-	if ( this.method == 'get' ) {
-		if ( this.parameters == "" )
+	if ( this.method === 'get' ) {
+		if ( this.parameters === "" )
 			this.xmlhttp.open("GET",this.url,true);
 		else
 			this.xmlhttp.open("GET",this.url + "?" + this.parameters,true);
